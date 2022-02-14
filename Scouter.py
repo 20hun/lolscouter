bc27e11087184ecc8dd5d6907c9aba383fa10b5a
danny <lyhgod@gmail.com>
2022-02-14 20:52:53 +0900

    add readme picture

--- Scouter.py
+++ Scouter.py
@@ -106,7 +106,7 @@ def version_check(version):
         soup = BeautifulSoup(html, 'html.parser')
 
         # 최근 20판 전적 = div.GameItemList
-        last_version = soup.select_one('#readme > div.Box-body.px-5.pb-5 > article > h1')
+        last_version = soup.select_one('#readme > div.Box-body.px-5.pb-5 > article > p:nth-child(5)')
 
         if last_version.text == version:
             return True
